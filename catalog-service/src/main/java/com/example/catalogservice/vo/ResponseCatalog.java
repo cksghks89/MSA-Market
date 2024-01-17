0faf0a3e605a3415c83fd6905f7c51bf03af894a
cksghks89 <song960705@naver.com>
2024-01-17 18:21:16 +0900

    feat: catalog-service init

---
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -0,0 +1,18 @@
+package com.example.catalogservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)  // null 값은 반환하지 않음
+public class ResponseCatalog {
+
+    private String productId;
+    private String productName;
+    private Integer unitPrice;
+    private Integer stock;
+    private Date createAt;
+
+}
